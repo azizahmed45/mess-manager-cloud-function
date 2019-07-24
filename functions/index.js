fc21f53f82021bc8d17fffd8866a87a4d38e8883
Aziz Ahmed <azizahmedsabuj@gmail.com>
2019-07-24 21:29:39 +0600

    Added deposit request notification to admin

--- functions/index.js
+++ functions/index.js
@@ -4,47 +4,42 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
-exports.myFunction = functions.firestore.document('Mess/{uniqueId}/Deposit/{depositId}')
-.onCreate((snap, context) => {
-
-  console.log(snap.data().userUid);
-
-	// if (context.params.deposiId && snap.data()) {
-    const uid = snap.data().userUid;
-
+exports.myFunction = functions.firestore.document('Mess/{uniqueId}/Deposit/{depositId}').onCreate((snap, context) => {
+	return (
+		//get admin uid
 		db
-			.collection(`Users/${uid}/Device`)
-			.listDocuments()
-			.then((docRef) => {
-				
-				db
-					.getAll(...docRef)
-					.then((docs) => {
-
-            console.log('Works');
-
-						for (let documentSnapshot of docs) {
-							if (documentSnapshot.exists) {
-                console.log(documentSnapshot);
-              
-								sendNotifications(documentSnapshot.get('token'), snap.data.amount);
-							} else {
-								console.log(`Found missing document: ${documentSnapshot.id}`);
-							}
-						}
-					})
-					.catch((error) => console.log(error));
+			.doc(`Mess/${context.params.uniqueId}`)
+			.get()
+			.then((documentSnapshot) => {
+				return documentSnapshot.get('adminUid');
 			})
-			.catch((error) => console.log(error));
-	// }
+			//get admin devices reference
+			.then((adminUid) => {
+				return db.collection(`Users/${adminUid}/Device`).listDocuments();
+			})
+			//get admin devices doc
+			.then((documnetReferences) => {
+				return db.getAll(...documnetReferences);
+			})
+			//get admin devices
+			.then((documentSnapshots) => {
+				for (let documentSnapshot of documentSnapshots) {
+					if (documentSnapshot.exists) {
+						sendNotifications(documentSnapshot.get('token'), snap.data().amount);
+					} else {
+						console.log(`Found missing document: ${documentSnapshot.id}`);
+					}
+				}
+			})
+	);
 });
 
 function sendNotifications(token, amount) {
 	let message = {
 		data: {
-			"id": "130",
-			"title": "Deposit " + amount,
-			"body": "body"
+			id: '130',
+			title: 'Deposit ' + amount,
+			body: 'body'
 		},
 		token: token
 	};
